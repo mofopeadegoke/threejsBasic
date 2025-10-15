eb4b89350610b5e244d61915ef378888333aca8e
mofopeadegoke <mofopeadegoke@gmail.com>
2025-10-15 11:27:22 +0300

    wireframe geometry added

--- index.js
+++ index.js
@@ -14,15 +14,29 @@ camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geo = new THREE.OctahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-    color: 0xccff
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
 })
 
-const mesh = new THREE.Mesh(geo, mat)
-scene.add(mesh);
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+})
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
 
-function animate() {
+const hemi = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+
+
+const mesh = new THREE.Mesh(geo, mat)
+mesh.add(wireMesh);
+scene.add(mesh);
+scene.add(hemi)
+
+function animate(t = 0) {
     requestAnimationFrame(animate)
+    mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
 }
 
